43a99923e2d5dfb091f8fcaa177a08f18f7d5532
ZJW___ <35996429+Mmhmmmmm@users.noreply.github.com>
2021-03-13 20:42:42 +0800

    Add files via upload

--- mytest.py
+++ mytest.py
@@ -6,18 +6,20 @@ from data import CelebA
 import torch
 import torch.utils.data as data
 import torchvision.utils as vutils
+import numpy as np
+import cv2
 
 from attgan import AttGAN
 from data import check_attribute_conflict
-import cv2
+import torch.nn.functional as F
 
 def parse(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('--experiment_name', dest='experiment_name', type=str, default='06-36AM on March 09, 2021')
-    parser.add_argument('--gpu', dest='gpu', type=bool, default=True)
-    parser.add_argument('--use_model', dest='use_model', type=str, default='generator')
-    parser.add_argument('--data_path', dest='data_path', type=str, default='data\CelebA\Img\img_align_celeba')
-    parser.add_argument('--attr_path', dest='attr_path', type=str, default='data\CelebA\Anno\list_attr_celeba.txt')
+    parser.add_argument('--gpu', dest='gpu', type=bool, default=False)
+    parser.add_argument('--use_model', dest='use_model', type=str, default='discriminator')
+    parser.add_argument('--data_path', dest='data_path', type=str, default='E:\personal_library\learning_in_vision\GAN_learning\code\CAFEGAN\CAFEGAN\data\CelebA\Img\img_align_celeba\img_align_celeba')
+    parser.add_argument('--attr_path', dest='attr_path', type=str, default='E:\personal_library\learning_in_vision\GAN_learning\code\CAFEGAN\CAFEGAN\data\CelebA\Anno\list_attr_celeba.txt')
     return parser.parse_args(args)
 
 args_ = parse()
@@ -49,8 +51,12 @@ for idx, (img_real, att_org) in enumerate(test_dataloader):
     img_real = img_real.cuda() if args.gpu else img_real
     att_org = att_org.cuda() if args.gpu else att_org
     att_org = att_org.type(torch.float)
-
+    _, mc, mw, mh = img_real.shape
     att_list = [att_org]
+    img_unit = img_real.view(3, mw, mh)
+    img_unit = ((img_unit * 0.5) + 0.5) * 255
+    img_unit = np.uint8(img_unit)
+    img_unit = img_unit[::-1,:,:].transpose(1,2,0)
     for i in range(args.n_attrs):
         tmp = att_org.clone()
         tmp[:, i] = 1 - tmp[:, i]
@@ -73,28 +79,25 @@ for idx, (img_real, att_org) in enumerate(test_dataloader):
             print('{:s} done!'.format(out_file))
     elif args_.use_model == 'discriminator':
         with torch.no_grad():
-            samples = []
+            result = img_unit
             abn_att_real, cabn_att_real = attgan.D(img_real, 'abn')
-            # num,  w, h = abn_att_real[0].size()
-            print(abn_att_real.shape)
-            
-            #print(att_org)
-            #print(len(att_list))
-            print(abn_att_real,cabn_att_real)
-            '''
-            for i in range(13):
-               # print(i)
-                abn_att = abn_att_real[0,i,:,:]
-                cabn_att = cabn_att_real[0,i,:,:]
-                # abn_att = cv2.resize(abn_att,(num, w, h))
-                print(1-att_org[0,i],cabn_att.min(),cabn_att.max(),cabn_att.mean(),cabn_att.std())
-                samples.append(cabn_att)
-            samples = torch.cat(samples, dim=1)
-            out_file = '{:06d}_c.jpg'.format(idx)
-            vutils.save_image(
-                samples, join(output_path, out_file),
-                nrow=1, normalize=True, range=(0., 1.)
-            )
+            for i in range(len(att_list)-1):
+                abn_att = F.interpolate(abn_att_real[i], size=(mw, mh), mode='bilinear', align_corners=True)
+                cabn_att = F.interpolate(cabn_att_real[i], size=(mw, mh), mode='bilinear', align_corners=True)
+                abn_att = (abn_att - abn_att.min()) / (abn_att.max() - abn_att.min())
+                cabn_att = (cabn_att - cabn_att.min()) / (cabn_att.max() - cabn_att.min())
+
+                abn_att = np.uint8(abn_att.view(mw, mh) * 255)
+                cabn_att = np.uint8(cabn_att.view(mw, mh) * 255)
+                heatmap_abn = cv2.applyColorMap(abn_att, cv2.COLORMAP_JET)
+                heatmap_cabn = cv2.applyColorMap(cabn_att, cv2.COLORMAP_JET)
+
+                result_abn = (heatmap_abn * 0.3 + img_unit * 0.5)
+                result_cabn = (heatmap_cabn * 0.3 + img_unit * 0.5)
+
+                result = np.append(result, result_abn, axis=1)
+                result = np.append(result, result_cabn, axis=1)
+
+            out_file = 'atts{:06d}.jpg'.format(idx)
+            cv2.imwrite(join(output_path, out_file), result)
             print('{:s} done!'.format(out_file))
-            
-            '''
